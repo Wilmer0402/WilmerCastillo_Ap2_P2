9a7416315f743f3cda3ea8d1a4a3ececf2290f10
Wilmer Miguel Castillo Tejada <wilmer_castillo@ucne.edu.do>
2025-06-18 20:24:37 -0400

    Configuracion del Hilt

---
+++ app/src/main/java/edu/ucne/wilmercastillo_ap2_p2/WilmerCastillo_AP2_P2App.kt
@@ -0,0 +1,7 @@
+package edu.ucne.wilmercastillo_ap2_p2
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class WilmerCastillo_AP2_P2App : Application()
