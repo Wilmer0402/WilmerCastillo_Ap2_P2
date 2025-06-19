999a73b256c536a3a705da76e0a97da12b0e3af8
Wilmer Miguel Castillo Tejada <wilmer_castillo@ucne.edu.do>
2025-06-19 11:00:56 -0400

    Modificando RepositoryUiState para Get de Repository

--- app/src/main/java/edu/ucne/wilmercastillo_ap2_p2/presentation/repository/RepositoryUiState.kt
+++ app/src/main/java/edu/ucne/wilmercastillo_ap2_p2/presentation/repository/RepositoryUiState.kt
@@ -1,4 +1,11 @@
 package edu.ucne.wilmercastillo_ap2_p2.presentation.repository
 
-class RepositoryUiState {
-}
+import edu.ucne.wilmercastillo_ap2_p2.data.remote.dto.RepositoryDto
+
+data class RepositoryUiState (
+    val name: String = "",
+    val descripcion: String = "",
+    val html_url: String = "",
+    val isLoading: Boolean = false,
+    val repository: List<RepositoryDto> = emptyList()
+)
