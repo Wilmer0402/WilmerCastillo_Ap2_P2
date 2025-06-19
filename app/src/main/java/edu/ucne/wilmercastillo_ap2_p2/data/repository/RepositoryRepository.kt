14b31f1fb4cc08569584f4ecf1e491416ee30039
Wilmer Miguel Castillo Tejada <wilmer_castillo@ucne.edu.do>
2025-06-19 11:00:31 -0400

    Modificando RepositoryRepository para Get de Repository

--- app/src/main/java/edu/ucne/wilmercastillo_ap2_p2/data/repository/RepositoryRepository.kt
+++ app/src/main/java/edu/ucne/wilmercastillo_ap2_p2/data/repository/RepositoryRepository.kt
@@ -10,10 +10,10 @@ import javax.inject.Inject
 class RepositoryRepository @Inject constructor(
     private val remoteDataSource: RemoteDataSource
 ) {
-    suspend fun getRepositories(username: String): Flow<Resource<List<RepositoryDto>>> = flow {
+    fun getRepositories(username: String): Flow<Resource<List<RepositoryDto>>> = flow {
         emit(Resource.Loading())
         try {
-            val repositories = remoteDataSource.getRepositories(username)
+            val repositories = remoteDataSource.getRepository(username)
             emit(Resource.Success(repositories))
         } catch (e: Exception) {
             emit(Resource.Error("Error al obtener repositorios: ${e.message}"))
