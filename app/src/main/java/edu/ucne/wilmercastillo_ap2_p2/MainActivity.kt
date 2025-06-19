dede9d097115ed826d26c309af8f35e02b6378aa
Wilmer Miguel Castillo Tejada <wilmer_castillo@ucne.edu.do>
2025-06-19 10:59:06 -0400

    Modificando MainActivity para Get de Repository

--- app/src/main/java/edu/ucne/wilmercastillo_ap2_p2/MainActivity.kt
+++ app/src/main/java/edu/ucne/wilmercastillo_ap2_p2/MainActivity.kt
@@ -6,12 +6,16 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import dagger.hilt.android.AndroidEntryPoint
+import edu.ucne.wilmercastillo_ap2_p2.presentation.repository.RepositoryListScreen
 import edu.ucne.wilmercastillo_ap2_p2.ui.theme.WilmerCastillo_Ap2_P2Theme
 
 @AndroidEntryPoint
@@ -21,29 +25,16 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             WilmerCastillo_Ap2_P2Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+                val scope = rememberCoroutineScope()
+
+                RepositoryListScreen(
+                    drawerState = drawerState,
+                    scope = scope
+
+                )
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    WilmerCastillo_Ap2_P2Theme {
-        Greeting("Android")
-    }
-}
