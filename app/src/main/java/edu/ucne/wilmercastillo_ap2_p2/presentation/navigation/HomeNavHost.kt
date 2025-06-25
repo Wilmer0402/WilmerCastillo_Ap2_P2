a6699603692581df5f72675a882f53d276f24d22
Wilmer Miguel Castillo Tejada <wilmer_castillo@ucne.edu.do>
2025-06-25 12:28:29 -0400

    Modificando HomeNavHost

--- app/src/main/java/edu/ucne/wilmercastillo_ap2_p2/presentation/navigation/HomeNavHost.kt
+++ app/src/main/java/edu/ucne/wilmercastillo_ap2_p2/presentation/navigation/HomeNavHost.kt
@@ -1,2 +1,36 @@
 package edu.ucne.wilmercastillo_ap2_p2.presentation.navigation
 
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.rememberDrawerState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import edu.ucne.wilmercastillo_ap2_p2.presentation.repository.RepositoryListScreen
+
+@Composable
+fun HomeNavHost(
+    navHostController: NavHostController,
+){
+
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
+    NavHost(
+        navController = navHostController,
+        startDestination = Screen.RepositoryList
+    ) {
+        composable<Screen.RepositoryList> {
+            RepositoryListScreen(
+                goToRepository = { id ->
+                    navHostController.navigate(Screen.Repository(null))
+                },
+                createRepository = {
+                    navHostController.navigate(Screen.Repository(null))
+                },
+                drawerState = drawerState,
+                scope = scope
+            )
+        }
+    }
+}
