335188141bfbcb6a7892b645bb5bd6e30eb5ecbd
Wilmer Miguel Castillo Tejada <wilmer_castillo@ucne.edu.do>
2025-06-25 12:28:04 -0400

    Modificando MainActivity

--- app/src/main/java/edu/ucne/wilmercastillo_ap2_p2/MainActivity.kt
+++ app/src/main/java/edu/ucne/wilmercastillo_ap2_p2/MainActivity.kt
@@ -4,18 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.rememberDrawerState
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
-import edu.ucne.wilmercastillo_ap2_p2.presentation.repository.RepositoryListScreen
+import edu.ucne.wilmercastillo_ap2_p2.presentation.navigation.HomeNavHost
 import edu.ucne.wilmercastillo_ap2_p2.ui.theme.WilmerCastillo_Ap2_P2Theme
 
 @AndroidEntryPoint
@@ -25,14 +21,16 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             WilmerCastillo_Ap2_P2Theme {
-                val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
-                val scope = rememberCoroutineScope()
-
-                RepositoryListScreen(
-                    drawerState = drawerState,
-                    scope = scope
-
-                )
+             val navigation = rememberNavController()
+                Scaffold(
+                    modifier = Modifier.fillMaxSize()
+                ){ innerPadding ->
+                    Box(
+                        modifier = Modifier.padding(innerPadding)
+                    ){
+                        HomeNavHost(navigation)
+                    }
+                }
                 }
             }
         }
