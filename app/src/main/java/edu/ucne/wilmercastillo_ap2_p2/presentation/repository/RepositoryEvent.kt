3a58dcaa266314c2f0c2fb0dfa9a2d57607aec2b
Wilmer Miguel Castillo Tejada <wilmer_castillo@ucne.edu.do>
2025-06-25 12:33:24 -0400

    Modificando RepositoryEvent

--- app/src/main/java/edu/ucne/wilmercastillo_ap2_p2/presentation/repository/RepositoryEvent.kt
+++ app/src/main/java/edu/ucne/wilmercastillo_ap2_p2/presentation/repository/RepositoryEvent.kt
@@ -1,4 +1,8 @@
 package edu.ucne.wilmercastillo_ap2_p2.presentation.repository
 
-interface RepositoryEvent {
+ sealed interface RepositoryEvent {
+     data object PostRepository: RepositoryEvent
+     data object GetRepositories: RepositoryEvent
+     data object PutRepositories: RepositoryEvent
+     data  object DeleteRepositories: RepositoryEvent
 }
