94657f4d0f08c900c07640faf1fad0b619d518d5
Wilmer Miguel Castillo Tejada <wilmer_castillo@ucne.edu.do>
2025-07-09 21:58:30 -0400

    Modificacion en MainActivity

--- app/src/main/java/edu/ucne/wilmercastillo_ap2_p2/MainActivity.kt
+++ app/src/main/java/edu/ucne/wilmercastillo_ap2_p2/MainActivity.kt
@@ -11,29 +11,28 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
-import edu.ucne.wilmercastillo_ap2_p2.presentation.navigation.HomeNavHost
+import edu.ucne.alainagarcia_ap2_p2.presentation.navigation.HomeNavHost
 import edu.ucne.wilmercastillo_ap2_p2.ui.theme.WilmerCastillo_Ap2_P2Theme
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         enableEdgeToEdge()
         setContent {
-            WilmerCastillo_Ap2_P2Theme {
-             val navigation = rememberNavController()
+           WilmerCastillo_Ap2_P2Theme {
+                val nav = rememberNavController()
                 Scaffold(
                     modifier = Modifier.fillMaxSize()
-                ){ innerPadding ->
+                ) { innerPadding ->
                     Box(
                         modifier = Modifier.padding(innerPadding)
-                    ){
-                        HomeNavHost(navigation)
+                    ) {
+                        HomeNavHost(nav)
                     }
-                }
+
                 }
             }
         }
     }
-
+}
