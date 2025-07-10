87cf17a44bb7671a258eb66847fb901b00bcb63c
Wilmer Miguel Castillo Tejada <wilmer_castillo@ucne.edu.do>
2025-07-09 21:56:46 -0400

    Modificacion en RepositoryRepository

--- app/src/main/java/edu/ucne/wilmercastillo_ap2_p2/data/repository/RepositoryRepository.kt
+++ app/src/main/java/edu/ucne/wilmercastillo_ap2_p2/data/repository/RepositoryRepository.kt
@@ -2,6 +2,7 @@ package edu.ucne.wilmercastillo_ap2_p2.data.repository
 
 import edu.ucne.wilmercastillo_ap2_p2.data.remote.RemoteDataSource
 import edu.ucne.wilmercastillo_ap2_p2.data.remote.Resource
+import edu.ucne.wilmercastillo_ap2_p2.data.remote.dto.ContributorDto
 import edu.ucne.wilmercastillo_ap2_p2.data.remote.dto.RepositoryDto
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
@@ -10,14 +11,24 @@ import javax.inject.Inject
 class RepositoryRepository @Inject constructor(
     private val remoteDataSource: RemoteDataSource
 ) {
-    fun getRepositories(username: String): Flow<Resource<List<RepositoryDto>>> = flow {
+    suspend fun getRepositories(username: String): Flow<Resource<List<RepositoryDto>>> = flow {
         emit(Resource.Loading())
         try {
             val repositories = remoteDataSource.getRepository(username)
             emit(Resource.Success(repositories))
         } catch (e: Exception) {
             emit(Resource.Error("Error al obtener repositorios: ${e.message}"))
-            }
         }
+    }
+
+    suspend fun getContributors(owner: String, repo: String): Flow<Resource<List<ContributorDto>>> = flow {
+        emit(Resource.Loading())
+        try {
+            val contributors = remoteDataSource.getContributors(owner, repo)
+            emit(Resource.Success(contributors))
+        } catch (e: Exception) {
+            emit(Resource.Error("Error al obtener contribuidores: ${e.message}"))
+        }
+    }
 
 }
