1927187a9ccd2d5d669743aa69380b0e847c22dd
Wilmer Miguel Castillo Tejada <wilmer_castillo@ucne.edu.do>
2025-07-09 21:58:59 -0400

    Modificacion en HomeNavHost

--- app/src/main/java/edu/ucne/wilmercastillo_ap2_p2/presentation/navigation/HomeNavHost.kt
+++ app/src/main/java/edu/ucne/wilmercastillo_ap2_p2/presentation/navigation/HomeNavHost.kt
@@ -1,4 +1,4 @@
-package edu.ucne.wilmercastillo_ap2_p2.presentation.navigation
+package edu.ucne.alainagarcia_ap2_p2.presentation.navigation
 
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.rememberDrawerState
@@ -7,29 +7,52 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.toRoute
+import edu.ucne.wilmercastillo_ap2_p2.presentation.contributors.ContributorListScreen
+import edu.ucne.wilmercastillo_ap2_p2.presentation.navigation.Screen
 import edu.ucne.wilmercastillo_ap2_p2.presentation.repository.RepositoryListScreen
 
 @Composable
 fun HomeNavHost(
     navHostController: NavHostController,
-){
 
+    ) {
     val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
     val scope = rememberCoroutineScope()
     NavHost(
         navController = navHostController,
         startDestination = Screen.RepositoryList
     ) {
+
         composable<Screen.RepositoryList> {
             RepositoryListScreen(
-                goToRepository = { id ->
-                    navHostController.navigate(Screen.Repository(null))
+                goToRepository = { repoName ->
+                    navHostController.navigate(
+                        Screen.ContributorList(
+                            owner = "enelramon",
+                            repo = repoName
+                        )
+                    )
                 },
                 createRepository = {
                     navHostController.navigate(Screen.Repository(null))
                 },
                 drawerState = drawerState,
-                scope = scope
+                scope = scope,
+                goToContributors = { owner, repoName ->
+                    navHostController.navigate(Screen.ContributorList(owner, repoName))
+                }
+            )
+        }
+
+        composable<Screen.ContributorList> { backStackEntry ->
+            val owner = backStackEntry.toRoute<Screen.ContributorList>().owner
+            val repo = backStackEntry.toRoute<Screen.ContributorList>().repo
+
+            ContributorListScreen(
+                owner = owner,
+                repo = repo,
+                onBack = { navHostController.popBackStack() }
             )
         }
     }
