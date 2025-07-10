a91c28bc6e48dce1b61e5fc42a72663883e211fb
Wilmer Miguel Castillo Tejada <wilmer_castillo@ucne.edu.do>
2025-07-09 21:59:20 -0400

    Modificacion en Screen

--- app/src/main/java/edu/ucne/wilmercastillo_ap2_p2/presentation/navigation/Screen.kt
+++ app/src/main/java/edu/ucne/wilmercastillo_ap2_p2/presentation/navigation/Screen.kt
@@ -2,11 +2,13 @@ package edu.ucne.wilmercastillo_ap2_p2.presentation.navigation
 
 import kotlinx.serialization.Serializable
 
-sealed class Screen {
+sealed class Screen{
     @Serializable
     data class Repository(val Id: Int?): Screen()
 
     @Serializable
     data object RepositoryList: Screen()
 
+    @Serializable
+    data class ContributorList(val owner: String, val repo: String) : Screen()
 }
