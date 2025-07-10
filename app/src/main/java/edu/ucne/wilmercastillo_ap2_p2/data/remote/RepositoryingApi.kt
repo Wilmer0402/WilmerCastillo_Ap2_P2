b516ec062889ae84d70e0476435958675a0cbb0d
Wilmer Miguel Castillo Tejada <wilmer_castillo@ucne.edu.do>
2025-07-09 21:56:28 -0400

    Modificacion en RepositoryingApi

--- app/src/main/java/edu/ucne/wilmercastillo_ap2_p2/data/remote/RepositoryingApi.kt
+++ app/src/main/java/edu/ucne/wilmercastillo_ap2_p2/data/remote/RepositoryingApi.kt
@@ -1,10 +1,17 @@
 package edu.ucne.wilmercastillo_ap2_p2.data.remote
 
+import edu.ucne.wilmercastillo_ap2_p2.data.remote.dto.ContributorDto
 import edu.ucne.wilmercastillo_ap2_p2.data.remote.dto.RepositoryDto
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface RepositoryingApi {
     @GET("users/{username}/repos")
-   suspend fun listRepos(@Path("username") username: String): List<RepositoryDto>
+    suspend fun listRepos(@Path("username") username: String): List<RepositoryDto>
+
+    @GET("repos/{owner}/{repo}/contributors")
+    suspend fun listContributors(
+        @Path("owner") owner: String,
+        @Path("repo") repo: String
+    ): List<ContributorDto>
 }
